1839b9a8912001c08f412a5ea3f912d96fe311ca
thorben ortmann <thorben.ortmann@pwc.com>
2020-11-22 22:11:21 +0100

    implemented face and dog detection

--- dog_breed_classifier/detection/dog_detection.py
+++ dog_breed_classifier/detection/dog_detection.py
@@ -1,5 +1,23 @@
 from pathlib import Path
 
+import numpy as np
+from tensorflow.keras.applications.nasnet import NASNetMobile, preprocess_input
+from tensorflow.keras.preprocessing import image
+
+classifier = NASNetMobile(include_top=True, weights='imagenet')
+
 
 def detect_dog(img_path: Path) -> bool:
-    return True
+    # returns prediction vector for image located at img_path
+    img = preprocess_input(path_to_tensor(str(img_path)))
+    prediction = np.argmax(classifier.predict(img))
+    return (prediction <= 268) & (prediction >= 151)
+
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
--- dog_breed_classifier/detection/face_detection.py
+++ dog_breed_classifier/detection/face_detection.py
@@ -1,5 +1,14 @@
 from pathlib import Path
 
+import cv2
+import cv2.cv2 as cv
+
+
+face_detector = cv.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
 
 def detect_human_face(img_path: Path) -> bool:
-    return True
+    img = cv.imread(str(img_path))
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray)
+    return len(faces) > 0
