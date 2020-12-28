222ca6a5504ec0dddd2715f7eb3096aa82c352f7
thorben ortmann <thorben.ortmann@pwc.com>
2020-12-28 22:45:53 +0100

    adjusted detection logic and improved code style

--- dog_breed_classifier/detection/breed_detection.py
+++ dog_breed_classifier/detection/breed_detection.py
@@ -1,35 +1,39 @@
 from pathlib import Path
 import random
-from typing import List, Optional
+from typing import List
 
 import numpy as np
-from tensorflow.keras.models import Sequential
+from keras import backend as K
+from keras.models import load_model
 
+from dog_breed_classifier.detection.training import utils
+from dog_breed_classifier.detection.training.transfer_learning import preprocess_input
 from dog_breed_classifier import paths
-from dog_breed_classifier.detection.training import train_from_scratch
 
-
-def get_model() -> Optional[Sequential]:
-    if paths.TRANSFER_LEARNING_MODEL_WEIGHTS.is_file():
-        pass
-    elif paths.FROM_SCRATCH_MODEL_WEIGHTS.is_file():
-        custom_model = train_from_scratch.create_model()
-        custom_model.load_weights(str(paths.FROM_SCRATCH_MODEL_WEIGHTS))
-        return custom_model
-
-    else:
-        return None
-
-
-classifier: Optional[Sequential] = get_model()
-dog_names: List[str] = [dir_.name[4:] for dir_ in paths.DOG_IMAGES_TRAIN.iterdir()]
+FROM_SCRATCH_MODEL_IS_PRESENT: bool = paths.FROM_SCRATCH_MODEL.is_file()
+TRANSFER_LEARNING_MODEL_IS_PRESENT: bool = paths.TRANSFER_LEARNING_MODEL.is_file()
+DOG_NAMES: List[str] = [dir_.name[4:] for dir_ in paths.DOG_IMAGES_TRAIN.iterdir()]
 
 
 def detect_breed(img_path: Path) -> str:
-    if classifier:
-        tensor = train_from_scratch.path_to_tensor(str(img_path)).astype('float32') / 255
-        predicted_vector = classifier.predict(tensor)
-        return dog_names[np.argmax(predicted_vector)]
+    """
+    Detects the breed of the dog in the given image.
+    :param img_path: path to the image to classify.
+    :return: the name of the predicted dog breed.
+    """
+    if FROM_SCRATCH_MODEL_IS_PRESENT or TRANSFER_LEARNING_MODEL_IS_PRESENT:
+        tensor = utils.path_to_tensor(str(img_path)).astype('float32') / 255
+
+        if TRANSFER_LEARNING_MODEL_IS_PRESENT:
+            tensor = preprocess_input(tensor)
+            model = load_model(str(paths.TRANSFER_LEARNING_MODEL))
+        else:
+            model = load_model(str(paths.FROM_SCRATCH_MODEL))
+
+        predicted_vector = model.predict(tensor)
+        K.clear_session()
+        return DOG_NAMES[np.argmax(predicted_vector)]
+
     else:
         return f'Seems like you did not train a model yet. ' \
-               f'So I will make a random guess: {dog_names[random.randint(0, 132)]} ?'
+               f'So I will make a random guess: {DOG_NAMES[random.randint(0, 132)]} ?'
