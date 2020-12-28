c060ab45889e270fd53d398cc360fd2a0036459f
thorben ortmann <thorben.ortmann@pwc.com>
2020-12-28 22:45:13 +0100

    Refactored to use of Xception network and added clear session for web app

--- dog_breed_classifier/detection/dog_detection.py
+++ dog_breed_classifier/detection/dog_detection.py
@@ -1,23 +1,20 @@
 from pathlib import Path
 
 import numpy as np
-from tensorflow.keras.applications.nasnet import NASNetMobile, preprocess_input
-from tensorflow.keras.preprocessing import image
+from keras import backend as K
+from keras.applications.xception import Xception, preprocess_input
 
-classifier = NASNetMobile(include_top=True, weights='imagenet')
+from dog_breed_classifier.detection.training import utils
 
 
 def detect_dog(img_path: Path) -> bool:
-    # returns prediction vector for image located at img_path
-    img = preprocess_input(path_to_tensor(str(img_path)))
-    prediction = np.argmax(classifier.predict(img))
-    return (prediction <= 268) & (prediction >= 151)
-
-
-def path_to_tensor(img_path):
-    # loads RGB image as PIL.Image.Image type
-    img = image.load_img(img_path, target_size=(224, 224))
-    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
-    x = image.img_to_array(img)
-    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
-    return np.expand_dims(x, axis=0)
+    """
+    Checks whether the passed image contains a dog or not.
+    :param img_path: path to the image.
+    :return: whether the passed image contains a dog or not.
+    """
+    dog_detector = Xception(include_top=True, weights='imagenet')
+    img = preprocess_input(utils.path_to_tensor(str(img_path)))
+    prediction = np.argmax(dog_detector.predict(img))
+    K.clear_session()
+    return 151 <= prediction <= 268
