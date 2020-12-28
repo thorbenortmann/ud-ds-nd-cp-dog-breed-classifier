a96ad98aed179cc27eaafafa1f98776152ebbd22
thorben ortmann <thorben.ortmann@pwc.com>
2020-12-28 22:42:34 +0100

    improved code style

--- dog_breed_classifier/detection/face_detection.py
+++ dog_breed_classifier/detection/face_detection.py
@@ -3,11 +3,15 @@ from pathlib import Path
 import cv2
 import cv2.cv2 as cv
 
-
 face_detector = cv.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 
 def detect_human_face(img_path: Path) -> bool:
+    """
+    Checks whether the passed image contains a human face or not.
+    :param img_path: path to the image.
+    :return: whether the passed image contains a human face or not.
+    """
     img = cv.imread(str(img_path))
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(gray)
